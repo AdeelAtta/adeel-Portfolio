5c482e5ad4b4dbdb83c9b7008f5ad0b67aa38af9
AdeelAtta <adeelatta2000@gmail.com>
2026-02-01 14:01:46 +0500

    feat: update favicon for better branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,26 @@ export const metadata: Metadata = {
   authors: [{ name: "Adeel Atta", url: "https://adeelatta.dev" }],
   creator: "Adeel Atta",
   icons: {
-    icon: "/icon.svg",
-    apple: "/apple-icon.png",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
   },
   openGraph: {
     type: "website",
